f97eed16bb7af5f7b40bd53911836742827c1713
Ai2304 <85852675+Ai2304@users.noreply.github.com>
2024-07-09 17:56:25 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 let ShapeLists=[];
 ShapeLists.push(new ballShape());
 //ShapeLists.push(new circleShape());
-ShapeLists.push(new squareShape());
-ShapeLists.push(new caroShape());
+// ShapeLists.push(new squareShape());
+// ShapeLists.push(new caroShape());
 let ColorList=[];
 //ColorList.push(new basicColor());
 ColorList.push(new redColor());
 ColorList.push(new greenColor());
 ColorList.push(new blueColor());
-//ColorList.push(new yellowColor());
-//ColorList.push(new purpleColor());
+ColorList.push(new yellowColor());
+ColorList.push(new purpleColor());
 
 let Maingame=new game("Maingame");
 
